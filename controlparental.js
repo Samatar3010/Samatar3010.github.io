1f22a62ec589421a94ed529838c1d2ee981e1636
samatar3010 <samatar@hotmail.be>
2021-12-03 19:25:05 +0100

    J'ai ajouté un formulaire

---
+++ controlparental.js
@@ -0,0 +1,47 @@
+const espaceMessage = document.getElementById("message");
+
+const bouton = document.getElementById("bouton");
+
+const ageInput = document.getElementById("age");
+
+const parentalInput = document.getElementById("parental");
+
+let age;
+let isControlParentalActive;
+let ageMajorite = 18;
+
+function valider() {
+        espaceMessage.innerHTML = "Vous êtes autorisé à entrer";
+    }
+    
+function refuser() {
+    espaceMessage.innerHTML= "Cette espace est interdit aux mineurs";
+    }
+    
+function onConfirm(){
+    age = parseInt(ageInput.value);
+        
+    //Si la saisie n'est pas un nombre, on affiche un message d'erreur
+    if(isNaN(age)) {
+        alert("Ceci n'est pas un nombre");
+        return;
+    }
+    //On récupère la valeur du checkbox de contrôle parentale. "true" si coché ou "false" si décoché
+    isControlParentalActive = parentalInput.checked;
+
+
+    if(age < ageMajorite && isControlParentalActive){
+        refuser();
+    }else{
+        valider()
+    }
+    //On vide ici le champ de saisie
+    ageInput.value = " ";
+
+    } //la function onConfirme s'arrête ici 
+
+
+/*On écoute l'action de click sur le bouton et on
+appelle la fonction onConfirm */
+
+bouton.addEventListener("click", onConfirm);
